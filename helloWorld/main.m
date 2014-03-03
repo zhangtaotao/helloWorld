36021825a00c39d4033385610d40d222013bfa90
zhangtaotao <741221187@qq.com>
2014-03-03 09:48:48 +0800

    add files

---
+++ helloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  helloWorld
+//
+//  Created by Tony on 14-3-3.
+//  Copyright (c) 2014年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
